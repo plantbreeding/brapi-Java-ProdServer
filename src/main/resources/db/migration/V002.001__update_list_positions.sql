ea85b8c5f2cc6609e0d0940424a13965ab2acbc0
mlm483 <128052931+mlm483@users.noreply.github.com>
2024-09-19 13:36:36 -0400

    [BI-2304] - fixed migration

--- src/main/resources/db/migration/V002.001__update_list_positions.sql
+++ src/main/resources/db/migration/V002.001__update_list_positions.sql
@@ -1,7 +1,7 @@
 
 -- This migration updates existing list_item records based on DeltaBreed (Breeding Insight) specific fields.
 --
--- These are the list types, since the BJTS uses Java enums, they are stored as int in the database.
+-- These are the list types, the BJTS uses Java enums and stores ints in the database.
 -- 0: germplasm
 -- 1: markers
 -- 2: programs
@@ -12,53 +12,55 @@
 -- 7: observationVariables
 -- 8: samples
 
+DO
+$$
+BEGIN
+    -- Update germplasm list items, the goal is to use the order defined by the listEntryNumbers.
+    UPDATE
+        list_item
+    SET
+        position = subquery.position
+    FROM
+        (
+            SELECT
+                row_number() OVER (PARTITION BY li.list_id ORDER BY (g.additional_info->'listEntryNumbers'->>xr.external_reference_id)::int) AS position,
+                li.id AS list_item_id
+            FROM
+                list_item li
+                    JOIN list l ON li.list_id = l.id
+                    JOIN list_external_references ler ON l.id = ler.list_entity_id
+                    JOIN external_reference xr ON xr.id = ler.external_references_id AND xr.external_reference_source = 'breedinginsight.org/lists'
+                    JOIN germplasm g ON li.item = g.germplasm_name
+            WHERE
+                l.list_type = 0  -- 0 is germplasm
+            ORDER BY
+                l.id
+        ) AS subquery
+    WHERE
+        list_item.id = subquery.list_item_id
+    ;
 
--- Update germplasm list items, the goal is to use the order defined by the listEntryNumbers.
-UPDATE
-    list_item
-SET
-    position = subquery.position
-FROM
-    (
-        SELECT
-            row_number() OVER (PARTITION BY li.list_id ORDER BY (g.additional_info->'listEntryNumbers'->>xr.external_reference_id)::int) AS position,
-            li.id AS list_item_id
-        FROM
-            list_item li
+    -- Update all non-germplasm list items. There is no existing order to preserve, assign sequential position values arbitrarily.
+    UPDATE
+        list_item
+    SET
+        position = subquery.position
+    FROM
+        (
+            SELECT
+                row_number() OVER (PARTITION BY li.list_id) AS position,
+                li.id AS list_item_id
+            FROM
+                list_item li
                 JOIN list l ON li.list_id = l.id
-                JOIN list_external_references ler ON l.id = ler.list_entity_id
-                JOIN external_reference xr ON xr.id = ler.external_references_id AND xr.external_reference_source = 'breedinginsight.org/lists'
-                JOIN germplasm g ON li.item = g.germplasm_name
-        WHERE
-            l.list_type = 0  -- 0 is germplasm
-        ORDER BY
-            l.id
-    ) AS subquery
-WHERE
-    list_item.id = subquery.list_item_id
-;
+            WHERE
+                l.list_type != 0  -- 0 is germplasm, here we are addressing non-germplasm lists.
+            ORDER BY
+                l.id
+        ) AS subquery
+    WHERE
+        list_item.id = subquery.list_item_id
+    ;
 
--- Update all non-germplasm list items. There is no existing order to preserve, assign sequential position values arbitrarily.
-UPDATE
-    list_item
-SET
-    position = subquery.position
-FROM
-    (
-        SELECT
-            row_number() OVER (PARTITION BY li.list_id) AS position,
-            li.id AS list_item_id
-        FROM
-            list_item li
-                JOIN list l ON li.list_id = l.id
-                JOIN list_external_references ler ON l.id = ler.list_entity_id
-                JOIN external_reference xr ON xr.id = ler.external_references_id AND xr.external_reference_source = 'breedinginsight.org/lists'
-                JOIN germplasm g ON li.item = g.germplasm_name
-        WHERE
-            l.list_type != 0  -- 0 is germplasm, here we are addressing non-germplasm lists.
-        ORDER BY
-            l.id
-    ) AS subquery
-WHERE
-    list_item.id = subquery.list_item_id
-;
+END;
+$$;
