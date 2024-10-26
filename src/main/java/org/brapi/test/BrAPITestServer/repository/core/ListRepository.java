9ebbfb1faa58360e1388d3869b08f2c3bede105d
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2024-10-26 19:47:57 -0400

    modify list repository to include clause softDelete=falsee

--- src/main/java/org/brapi/test/BrAPITestServer/repository/core/ListRepository.java
+++ src/main/java/org/brapi/test/BrAPITestServer/repository/core/ListRepository.java
@@ -2,6 +2,20 @@ package org.brapi.test.BrAPITestServer.repository.core;
 
 import org.brapi.test.BrAPITestServer.model.entity.core.ListEntity;
 import org.brapi.test.BrAPITestServer.repository.BrAPIRepository;
+import org.brapi.test.BrAPITestServer.service.SearchQueryBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ListRepository extends BrAPIRepository<ListEntity, String>{
+    public Page<ListEntity> findAllBySearchAndNotDeleted(SearchQueryBuilder<ListEntity> searchQuery, Pageable pageReq);
+
+    @Query("SELECT l FROM ListEntity l WHERE l.id = :id AND l.softDeleted = false")
+    public Optional<ListEntity> findByIdNotDeletedNoAuthCheck(String id);
+
+    public Optional<ListEntity> findByIdAndNotDeleted(String id);
+
 }
