019166666bb7cb944b15d6e21fb3c43fa15aded4
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2024-10-28 18:18:54 -0400

    add hard delete of single trial

--- src/main/java/org/brapi/test/BrAPITestServer/repository/core/TraitRepository.java
+++ src/main/java/org/brapi/test/BrAPITestServer/repository/core/TraitRepository.java
@@ -2,7 +2,21 @@ package org.brapi.test.BrAPITestServer.repository.core;
 
 import org.brapi.test.BrAPITestServer.model.entity.pheno.TraitEntity;
 import org.brapi.test.BrAPITestServer.repository.BrAPIRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface TraitRepository extends BrAPIRepository<TraitEntity, String> {
-
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE ListEntity l SET l.softDeleted = :softDeleted WHERE l.id = :listId")
+//    int updateSoftDeletedStatus(@Param("listId") String listId, @Param("softDeleted") boolean softDeleted);
+//
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE ListEntity l SET l.softDeleted = :softDeleted WHERE l.id IN :listIds")
+//    int updateSoftDeletedStatusBatch(@Param("listIds") List<String> listIds, @Param("softDeleted") boolean softDeleted);
 }
